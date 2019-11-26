0a0e292f5e01ab305229aa533bc5c3a69d3627c2
ManonCbz <56023215+ManonCbz@users.noreply.github.com>
2019-11-26 16:27:20 +0100

    Add historique

--- index.js
+++ index.js
@@ -1,91 +1,292 @@
+var historique = document.getElementById('historique');
 var imageOrdi = document.getElementById('imageordi');
 
 var boutonPierre = document.getElementById('pierre');
 var boutonFeuille = document.getElementById('feuille');
 var boutonCiseaux = document.getElementById('ciseaux');
 
-boutonPierre.addEventListener('click', function () {                         // Bouton Pierre
+var blockVictoire = document.getElementById('victoire');
+var blockDefaite = document.getElementById('defaite');
+var score = document.getElementById('score');
+
+var victoire = 0;
+var defaite = 0;
+
+score.innerHTML= 'Score : ';
+blockVictoire.innerHTML = 'Victoires : ';
+blockDefaite.innerHTML = 'Défaites : ';
+
+
+// ================================================== Pierre ================================================== \\
+
+
+boutonPierre.addEventListener('click', function () {
 
     var image = document.getElementById('image');
     image.style.cssText="background: url('img/user-rock.png'); background-repeat: no-repeat";
-    console.log('Pierre');
 
     var choixJose = Math.floor(Math.random()*3) + 1;
+
     switch (choixJose) {
         case 1 :
-            console.log('José a choisi Pierre');
+
             imageOrdi.style.cssText="background: url('img/jose-rock.png'); background-repeat: no-repeat";
+
+            var histun = document.createElement('div');
+            histun.classList.add('hist1');
+            historique.appendChild(histun);
+
+            var histdeux = document.createElement('div');
+            histdeux.classList.add('imghist1');
+            histun.appendChild(histdeux);
+            histdeux.style.cssText="background: url('img/rock.png'); background-repeat: no-repeat";
+
+            var histtrois = document.createElement('div');
+            histtrois.innerHTML= 'V.S.';
+            histun.appendChild(histtrois);
+
+            var histquatre = document.createElement('div');
+            histquatre.classList.add('imghist2');
+            histun.appendChild(histquatre);
+            histquatre.style.cssText="background: url('img/rock.png'); background-repeat: no-repeat";
             break;
 
         case 2 :
-            console.log('José a choisi Feuille');
             imageOrdi.style.cssText="background: url('img/jose-paper.png'); background-repeat: no-repeat";
+            defaite ++;
+
+            var histun = document.createElement('div');
+            histun.classList.add('hist1');
+            historique.appendChild(histun);
+
+            var histdeux = document.createElement('div');
+            histdeux.classList.add('imghist1');
+            histun.appendChild(histdeux);
+            histdeux.style.cssText="background: url('img/rock.png'); background-repeat: no-repeat";
+
+            var histtrois = document.createElement('div');
+            histtrois.innerHTML= 'V.S.';
+            histun.appendChild(histtrois);
+
+            var histquatre = document.createElement('div');
+            histquatre.classList.add('imghist2');
+            histun.appendChild(histquatre);
+            histquatre.style.cssText="background: url('img/paper.png'); background-repeat: no-repeat";
+
             break;
 
         case 3 :
-            console.log('José a choisi Ciseaux');
             imageOrdi.style.cssText="background: url('img/jose-scissors.png'); background-repeat: no-repeat";
+            victoire ++;
+
+            var histun = document.createElement('div');
+            histun.classList.add('hist1');
+            historique.appendChild(histun);
+
+            var histdeux = document.createElement('div');
+            histdeux.classList.add('imghist1');
+            histun.appendChild(histdeux);
+            histdeux.style.cssText="background: url('img/rock.png'); background-repeat: no-repeat";
+
+            var histtrois = document.createElement('div');
+            histtrois.innerHTML= 'V.S.';
+            histun.appendChild(histtrois);
+
+            var histquatre = document.createElement('div');
+            histquatre.classList.add('imghist2');
+            histun.appendChild(histquatre);
+            histquatre.style.cssText="background: url('img/scissors.png'); background-repeat: no-repeat";
+
             break;
     }
+
+    blockVictoire.innerHTML = 'Victoires : ' + victoire;
+    blockDefaite.innerHTML = 'Défaites : ' + defaite;
+
+    var pourcentage = Math.round((victoire/(victoire + defaite))*100);
+    score.innerHTML= 'Score : ' + pourcentage + '%';
+
 });
 
 
-boutonFeuille.addEventListener('click', function () {                    // Bouton Feuille
+// ================================================== Feuille ================================================== \\
 
+
+boutonFeuille.addEventListener('click', function () {
 
     var image = document.getElementById('image');
     image.style.cssText="background: url('img/user-paper.png'); background-repeat: no-repeat";
 
-
-    console.log('Feuille');
-
     var choixJose = Math.floor(Math.random()*3) + 1;
+
     switch (choixJose) {
         case 1 :
-            console.log('José a choisi Pierre');
             imageOrdi.style.cssText="background: url('img/jose-rock.png'); background-repeat: no-repeat";
-            victoire = +1;
+            victoire ++;
+
+            var histun = document.createElement('div');
+            histun.classList.add('hist1');
+            historique.appendChild(histun);
+
+            var histdeux = document.createElement('div');
+            histdeux.classList.add('imghist1');
+            histun.appendChild(histdeux);
+            histdeux.style.cssText="background: url('img/paper.png'); background-repeat: no-repeat";
+
+            var histtrois = document.createElement('div');
+            histtrois.innerHTML= 'V.S.';
+            histun.appendChild(histtrois);
+
+            var histquatre = document.createElement('div');
+            histquatre.classList.add('imghist2');
+            histun.appendChild(histquatre);
+            histquatre.style.cssText="background: url('img/rock.png'); background-repeat: no-repeat";
+
             break;
 
         case 2 :
-            console.log('José a choisi Feuille');
             imageOrdi.style.cssText="background: url('img/jose-paper.png'); background-repeat: no-repeat";
+
+            var histun = document.createElement('div');
+            histun.classList.add('hist1');
+            historique.appendChild(histun);
+
+            var histdeux = document.createElement('div');
+            histdeux.classList.add('imghist1');
+            histun.appendChild(histdeux);
+            histdeux.style.cssText="background: url('img/paper.png'); background-repeat: no-repeat";
+
+            var histtrois = document.createElement('div');
+            histtrois.innerHTML= 'V.S.';
+            histun.appendChild(histtrois);
+
+            var histquatre = document.createElement('div');
+            histquatre.classList.add('imghist2');
+            histun.appendChild(histquatre);
+            histquatre.style.cssText="background: url('img/paper.png'); background-repeat: no-repeat";
+
             break;
 
         case 3 :
-            console.log('José a choisi Ciseaux');
             imageOrdi.style.cssText="background: url('img/jose-scissors.png'); background-repeat: no-repeat";
-            victoire = -1;
+            defaite ++;
+
+            var histun = document.createElement('div');
+            histun.classList.add('hist1');
+            historique.appendChild(histun);
+
+            var histdeux = document.createElement('div');
+            histdeux.classList.add('imghist1');
+            histun.appendChild(histdeux);
+            histdeux.style.cssText="background: url('img/paper.png'); background-repeat: no-repeat";
+
+            var histtrois = document.createElement('div');
+            histtrois.innerHTML= 'V.S.';
+            histun.appendChild(histtrois);
+
+            var histquatre = document.createElement('div');
+            histquatre.classList.add('imghist2');
+            histun.appendChild(histquatre);
+            histquatre.style.cssText="background: url('img/scissors.png'); background-repeat: no-repeat";
+
             break;
     }
+
+    blockVictoire.innerHTML = 'Victoires : ' + victoire;
+    blockDefaite.innerHTML = 'Défaites : ' + defaite;
+
+    var pourcentage = Math.round((victoire/(victoire + defaite))*100);
+    score.innerHTML= 'Score : ' + pourcentage + '%';
 });
 
 
+// ================================================== Ciseaux ================================================== \\
 
-boutonCiseaux.addEventListener('click', function () {                        // Bouton ciseaux
+
+boutonCiseaux.addEventListener('click', function () {
 
     var image = document.getElementById('image');
     image.style.cssText="background: url('img/user-scissors.png'); background-repeat: no-repeat";
 
-    console.log('Ciseaux');
-
     var choixJose = Math.floor(Math.random()*3) + 1;
+
     switch (choixJose) {
+
         case 1 :
-            console.log('José a choisi Pierre');
             imageOrdi.style.cssText="background: url('img/jose-rock.png'); background-repeat: no-repeat";
-            victoire = -1;
+            defaite ++;
+
+            var histun = document.createElement('div');
+            histun.classList.add('hist1');
+            historique.appendChild(histun);
+
+            var histdeux = document.createElement('div');
+            histdeux.classList.add('imghist1');
+            histun.appendChild(histdeux);
+            histdeux.style.cssText="background: url('img/scissors.png'); background-repeat: no-repeat";
+
+            var histtrois = document.createElement('div');
+            histtrois.innerHTML= 'V.S.';
+            histun.appendChild(histtrois);
+
+            var histquatre = document.createElement('div');
+            histquatre.classList.add('imghist2');
+            histun.appendChild(histquatre);
+            histquatre.style.cssText="background: url('img/rock.png'); background-repeat: no-repeat";
+
             break;
 
         case 2 :
-            console.log('José a choisi Feuille');
             imageOrdi.style.cssText="background: url('img/jose-paper.png'); background-repeat: no-repeat";
-            victoire = +1;
+            victoire ++;
+
+            var histun = document.createElement('div');
+            histun.classList.add('hist1');
+            historique.appendChild(histun);
+
+            var histdeux = document.createElement('div');
+            histdeux.classList.add('imghist1');
+            histun.appendChild(histdeux);
+            histdeux.style.cssText="background: url('img/scissors.png'); background-repeat: no-repeat";
+
+            var histtrois = document.createElement('div');
+            histtrois.innerHTML= 'V.S.';
+            histun.appendChild(histtrois);
+
+            var histquatre = document.createElement('div');
+            histquatre.classList.add('imghist2');
+            histun.appendChild(histquatre);
+            histquatre.style.cssText="background: url('img/paper.png'); background-repeat: no-repeat";
+
             break;
 
         case 3 :
-            console.log('José a choisi Ciseaux');
             imageOrdi.style.cssText="background: url('img/jose-scissors.png'); background-repeat: no-repeat";
+
+            var histun = document.createElement('div');
+            histun.classList.add('hist1');
+            historique.appendChild(histun);
+
+            var histdeux = document.createElement('div');
+            histdeux.classList.add('imghist1');
+            histun.appendChild(histdeux);
+            histdeux.style.cssText="background: url('img/scissors.png'); background-repeat: no-repeat";
+
+            var histtrois = document.createElement('div');
+            histtrois.innerHTML= 'V.S.';
+            histun.appendChild(histtrois);
+
+            var histquatre = document.createElement('div');
+            histquatre.classList.add('imghist2');
+            histun.appendChild(histquatre);
+            histquatre.style.cssText="background: url('img/scissors.png'); background-repeat: no-repeat";
+
             break;
     }
+
+    blockVictoire.innerHTML = 'Victoires : ' + victoire;
+    blockDefaite.innerHTML = 'Défaites : ' + defaite;
+
+    var pourcentage = Math.round((victoire/(victoire + defaite))*100);
+    score.innerHTML= 'Score : ' + pourcentage + '%';
 });
